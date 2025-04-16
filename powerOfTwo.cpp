0895861873bab6908c9884828bb6879dff4b38db
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-04-16 12:57:22 +0000

    Added powerOfTwo.cpp

---
+++ powerOfTwo.cpp
@@ -0,0 +1,50 @@
+// Copyright (c) 2024 Jayden Smith All rights reserved.
+// Created by: Jayden Smith
+// Date: April 6, 2024
+// This program calculates the factorial product of the user`s number
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+
+int main() {
+    int loopCounter;
+    int factorialAnswer = 1;
+    std::string userNum;
+
+    // get the user's number
+    std::cout << "Enter a whole number: ";
+    std::cin >> userNum;
+
+    try {
+        // Converts userNum to integer
+        int userNumAsInt = std::stoi(userNum);
+        float userNumAsFloat = std::stof(userNum);
+
+        // Checks if userNum is a decimal
+        if (userNumAsInt != userNumAsFloat) {
+            std::cout << userNumAsFloat << " is not an integer!" << std::endl;
+
+            // Checks if userNum is negative
+        } else if (userNumAsInt < 0) {
+            std::cout << userNumAsInt <<
+            " is not a positive integer!" << std::endl;
+
+            // Checks if userNum is a 0
+        } else if (userNumAsInt == 0) {
+            std::cout << userNumAsInt << "! = 1" << std::endl;
+
+            // If none of those, calculates squares up to the users number
+        } else {
+            for (loopCounter = 0; loopCounter <= userNumAsInt; loopCounter++) {
+            std::cout << loopCounter <<
+            "^2 = " << pow(loopCounter , 2) << std::endl;
+        }
+
+        // What happens if it is not a valid integer
+        }
+    } catch (std::invalid_argument) {
+        std::cerr << userNum << " is not a valid integer." << std::endl;
+    }
+}
