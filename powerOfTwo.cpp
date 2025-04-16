940d1c131ac61b12ebb011bb7c225943077b23a9
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-04-16 12:57:54 +0000

    Added powerOfTwo.cpp

--- powerOfTwo.cpp
+++ powerOfTwo.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2024 Jayden Smith All rights reserved.
 // Created by: Jayden Smith
 // Date: April 6, 2024
-// This program calculates the factorial product of the user`s number
+// This program calculates the square of every number up to the user`s number
 
 #include <iostream>
 #include <string>
